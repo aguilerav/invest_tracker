5ed56824dc8e8b5b8bd61e4a6e597bcbf933bbed
Vicente Aguilera <vicente.aguilerae@gmail.com>
2025-05-01 01:17:55 -0400

    Add transactions and tickers tables

--- test/dummy_ddbb.py
+++ test/dummy_ddbb.py
@@ -5,6 +5,7 @@ This codes creates a dummy database in csv
 import pandas as pd
 from ahorro.utils.paths import DATA_DIR, PROJECT_ROOT
 
+# Users for login
 users = pd.DataFrame(
     {
         "user_id": [1, 2, 3],
@@ -14,6 +15,7 @@ users = pd.DataFrame(
 )
 users.to_csv(DATA_DIR / "users_plain.csv", index=False)
 
+# Clients information
 clients = pd.DataFrame(
     {
         "client_id": [1, 2, 3],
@@ -22,3 +24,26 @@ clients = pd.DataFrame(
     }
 )
 clients.to_csv(DATA_DIR / "clients.csv", index=False)
+
+# Tickers
+tickers = pd.DataFrame(
+    {
+        "ticker_id": [1, 2, 3],
+        "ticker_name": ["Apple", "Alphabet", "Microsoft"],
+        "ticker_symbol": ["AAPL", "GOOGL", "MSFT"],
+    }
+)
+tickers.to_csv(DATA_DIR / "tickers.csv", index=False)
+
+# Transactions information
+trx = pd.DataFrame(
+    {
+        "trx_id": [1, 2, 3, 4, 5],
+        "user_id": [1, 1, 1, 2, 3],
+        "amount": [200.0, 100.0, 300.0, 50.0, 150.0],
+        "type": ["buy", "sell", "buy", "buy", "buy"],
+        "date": ["2024-12-01", "2024-12-12", "2024-12-20", "2024-12-10", "2024-12-09"],
+        "ticker_id": [1, 1, 2, 3, 1],
+    }
+)
+trx.to_csv(DATA_DIR / "transactions.csv", index=False)
